8c0100e4a20054dba59858dd1d3d8cf9aa3e9773
Emily <fehlviimusics@gmail.com>
2026-05-05 21:21:49 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema Login</title>
+</head>
+<body>
+
+<div>
+    <h3>Entre</h3> 
+    <hr>
+
+<form action="SrvLogin.php" method="POST">
+      <h3>Login</h3>
+    <input type="text" name="login" placeholder="Digite seu login"/>
+      
+    <br><br>
+    <h3>Senha</h3>
+    <input type="password" name="senha" placeholder="Digite seu Senha"/>
+    <br><br>
+    <hr>
+    <button type="submit">Acessar</button> 
+        <button type="submit">Limpar</button> 
+</form>
+</div>
+
+</body>
+</html>
